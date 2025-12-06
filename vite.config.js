b5797f5a43da45005a7611cadfa72e0c60160fb3
brunalmeidas <alm.brunaa@gmail.com>
2025-12-05 22:34:47 -0300

    feat: configuração inicial do projeto com Vite e React

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Configuração do Vite para o projeto React
+export default defineConfig({
+  plugins: [react()],
+})
+
